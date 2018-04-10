e0682a6a496c51319d69d5e78227d62cb4974510
MarcoJL066 <38052746+MarcoJL066@users.noreply.github.com>
2018-04-10 12:50:42 -0500

    Update Mensaje.java

--- src/Mensaje.java
+++ src/Mensaje.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-	System.out.println("Master mode");
+	System.out.println("Master mode, it's extremely hard");
 	System.out.println("Modifying in master branch");
 
 	}
