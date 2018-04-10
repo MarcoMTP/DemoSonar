b0b3b65fedc97af60c4d6266ac30060f99fcea1a
MarcoMTP <38253938+MarcoMTP@users.noreply.github.com>
2018-04-10 11:14:03 -0500

    Update Mensaje.java

--- src/Mensaje.java
+++ src/Mensaje.java
@@ -6,7 +6,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
 
- 	System.out.println("...");
 	System.out.println("Modifying in Branch again");
 
 	}
